6b90db113ed99ddbe89240648a47250b1693c8c2
Mateusz Twaróg <mateusz.twarog@deepsense.ai>
2025-12-12 11:20:12 +0100

    test

--- src/racetrack_job_wrapper/endpoint_config.py
+++ src/racetrack_job_wrapper/endpoint_config.py
@@ -1,10 +1,11 @@
 from dataclasses import dataclass
 from http import HTTPMethod
-from typing import Callable
+from typing import Any, Callable, Dict
 
 
 @dataclass
 class EndpointConfig:
     path: str
-    method: HTTPMethod 
+    method: HTTPMethod
     handler: Callable
+    other_options: Dict[str, Any] = {}
